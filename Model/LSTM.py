75020c01611f2bf1e928d4fe1b72d12617ae247a
youtipie <youtipie@gmail.com>
2025-03-24 14:27:27 +0200

    fix lstm

--- Model/LSTM.py
+++ Model/LSTM.py
@@ -10,12 +10,14 @@ class LSTM:
     def __init__(
             self,
             sequence_length=60,
+            prediction_steps=10,
             train_split=0.95,
             features=None,
             target='Close',
             datapath='../datasets/BTC_ready.csv'
     ):
         self.sequence_length = sequence_length
+        self.prediction_steps = prediction_steps
         self.train_split = train_split
         self.features = features if features else ['Close']
         self.target = target
@@ -23,7 +25,7 @@ class LSTM:
         self.data = self.load_data(datapath)
         self.model = self.build_model()
 
-    def predict(self, num_rows):
+    def predict(self):
         scaled_data, scaled_target, _, scaler, target_scaler = self.preprocess_data(
             self.data,
             self.features,
@@ -31,20 +33,12 @@ class LSTM:
         )
 
         current_sequence = scaled_data[-self.sequence_length:].copy()
+        input_sequence = current_sequence.reshape(1, self.sequence_length, len(self.features))
 
-        predictions_scaled = []
+        pred_scaled = self.model.predict(input_sequence, verbose=0)
 
-        for _ in range(num_rows):
-            input_sequence = current_sequence.reshape(1, self.sequence_length, len(self.features))
-            pred_scaled = self.model.predict(input_sequence, verbose=0)
-            predictions_scaled.append(pred_scaled[0, 0])
-
-            new_row = np.array([[pred_scaled[0, 0]]])
-            current_sequence = np.append(current_sequence, new_row, axis=0)
-            current_sequence = current_sequence[1:]
-
-        predictions_scaled = np.array(predictions_scaled).reshape(-1, 1)
-        predictions = target_scaler.inverse_transform(predictions_scaled)
+        pred_scaled_reshaped = pred_scaled.reshape(-1, 1)
+        predictions = target_scaler.inverse_transform(pred_scaled_reshaped)
         return predictions
 
     def train(self, epochs=10, batch_size=32, verbose=False):
@@ -65,20 +59,27 @@ class LSTM:
             test_target = scaled_target[training_data_len - self.sequence_length:]
             X_test, y_test = self.create_sequences(test_data, test_target)
 
-            predictions = self.model.predict(X_test)
-            predictions = target_scaler.inverse_transform(predictions)
+            predictions = self.model.predict(X_test[::self.prediction_steps])
+            predictions = target_scaler.inverse_transform(predictions.reshape(-1, 1))
 
-            test = self.data[training_data_len:].copy()
-            test['Predictions'] = predictions
-            self.plot_predictions(test, predictions)
+            selected_y = y_test[::self.prediction_steps]
+            nsamples, nx, ny = selected_y.shape
+            reshaped_y = selected_y.reshape(nsamples, nx * ny).reshape(-1, 1)
+            actual = target_scaler.inverse_transform(reshaped_y)
+
+            self.plot_predictions(actual, predictions)
 
     def build_model(self):
         model = keras.models.Sequential()
         model.add(keras.layers.LSTM(64, return_sequences=True, input_shape=(self.sequence_length, len(self.features))))
+        model.add(keras.layers.LSTM(64, return_sequences=True))
+        model.add(keras.layers.Dropout(0.2))
         model.add(keras.layers.LSTM(64, return_sequences=False))
         model.add(keras.layers.Dense(128, activation='relu'))
         model.add(keras.layers.Dropout(0.5))
-        model.add(keras.layers.Dense(1))
+        model.add(keras.layers.Dense(128, activation='relu'))
+        model.add(keras.layers.Dropout(0.5))
+        model.add(keras.layers.Dense(self.prediction_steps))
 
         model.compile(optimizer="adam",
                       loss="mae",
@@ -87,11 +88,10 @@ class LSTM:
 
     def create_sequences(self, data, target):
         X, y = [], []
-        for i in range(self.sequence_length, len(data)):
+        for i in range(self.sequence_length, len(data) - self.prediction_steps + 1):
             X.append(data[i - self.sequence_length:i])
-            y.append(target[i])
-        X, y = np.array(X), np.array(y)
-        return X, y
+            y.append(target[i:i + self.prediction_steps])
+        return np.array(X), np.array(y)
 
     def preprocess_data(self, data, features, target='Close'):
         feature_data = data[features]
@@ -106,25 +106,31 @@ class LSTM:
         return scaled_data, scaled_target, training_data_len, scaler, target_scaler
 
     @staticmethod
-    def load_data(path):
-        return pd.read_csv(path)
-
-    @staticmethod
-    def plot_predictions(test, predictions, date_column='Open time', target_column='Close'):
+    def plot_predictions(test, predictions):
         plt.figure(figsize=(12, 8))
-        plt.plot(test[date_column], test[target_column], label='Test (Actual)', color='orange')
-        plt.plot(test[date_column], predictions, label='Predictions', color='red')
+        plt.plot(test, label='Test (Actual)', color='orange')
+        plt.plot(predictions, label='Predictions', color='red')
         plt.title("Stock Price Prediction")
-        plt.xlabel("Date")
+        plt.xlabel("Time")
         plt.ylabel("Close Price")
         plt.legend()
         plt.show()
 
+    @staticmethod
+    def load_data(path):
+        return pd.read_csv(path)
+
 
 if __name__ == '__main__':
-    model = LSTM()
+    features = [
+        "Close", "Quote asset volume",
+        "Number of trades", "Taker buy base asset volume",
+        "Taker buy quote asset volume",
+        "Score", "SMA_20", "RSI_14", "RSI_7", "MACD_14"
+    ]
+
+    model = LSTM(prediction_steps=20, features=features)
     model.train(20, verbose=True)
-    # predictions = model.predict(10)
-    # plt.figure(figsize=(12, 8))
-    # plt.plot(predictions)
-    # plt.show()
+    plt.plot(model.predict())
+    plt.title("Predictions")
+    plt.show()
